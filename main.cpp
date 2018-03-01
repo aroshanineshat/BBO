81be063275dd199f216ec921c7f118847491fac8
Arash Roshanineshat <a.roshanineshat@vikes.csuohio.edu>
2018-03-01 04:29:46 -0500

    final version before debug

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <array>
 #include <iostream>
 #include <random>
 #include <cmath>
@@ -9,21 +10,22 @@ void Data_Sort(std::vector<std::vector<float>>&, std::vector<float>&, int);
 int main (int argc, char* argv[])
 {
 
-    int GenerationLimit=40;
-    int PopulationSize=20;
-    int PopulationDimention=50;
-    float MutationProbability=0.04;
-    int NumberOfElites=2;
-
+    const int GenerationLimit=40;
+    const int PopulationSize=20;
+    const int PopulationDimention=50;
+    const float MutationProbability=0.04;
+    const int NumberOfElites=2;
+    std::cout<< "1";
     std::vector<float> Minimum_Range;
     std::vector<float> Maximum_Range;
+    std::vector<float> Minimum_Cost;
 
     Minimum_Range.push_back(-2.048);
     Maximum_Range.push_back(+2.048);
 
     std::random_device rd;
     std::mt19937 gen(rd());
-
+    std::cout << "stating";
     std::vector<std::vector<float>> x;
 
     for (int i =0; i< PopulationSize; i++)
@@ -41,7 +43,89 @@ int main (int argc, char* argv[])
 
     std::vector<float> Cost=Cost_Function(x, PopulationSize, PopulationDimention);
     Data_Sort(x, Cost, PopulationSize);
+    Minimum_Cost.push_back(Cost[0]);
+    std::cout<<"Generation 0 minimum cost: " << Minimum_Cost[0];
 
+    std::vector<std::vector<float>> z;
+    std::array<float, PopulationSize> mu;
+    std::array<float, PopulationSize> lambda;
+    float mu_sum =0;
+    std::vector<std::vector<float>> EliteSolutions;
+    std::vector<float> EliteCosts;
+    for (int i=0; i < PopulationSize; i++)
+    {
+        mu[i]=(PopulationSize-i)/(PopulationSize+1);
+        mu_sum=mu_sum+mu[i];
+        lambda[i]=1-mu[i];
+    }
+
+    for (int crnt_generation=0; crnt_generation<GenerationLimit; crnt_generation++)
+    {
+        EliteSolutions.clear();
+        EliteCosts.clear();
+        for (int i =0; i< NumberOfElites;i++)
+        {
+            EliteSolutions.push_back(x[i]);
+            EliteCosts.push_back(Cost[i]);
+        }
+        for (int k=0; k< PopulationSize; k++)
+        {
+            std::vector<float> z_temp;
+            for (int j=0; j<PopulationDimention; j++)
+            {
+                std::uniform_real_distribution<> migration_rate (0,1);
+                if (migration_rate(gen) < lambda[k])
+                {
+                    std::uniform_real_distribution<> randomnm(0,1);
+                    float random_a = randomnm(gen) * mu_sum;
+                    float select = mu[0];
+                    int select_ind=0;
+                    while((random_a > select) && (select_ind < PopulationSize))
+                    {
+                        select_ind++;
+                        select = select + mu[select_ind];
+                    }
+                    z_temp.push_back(x[select_ind][j]);
+                }else{
+                    z_temp.push_back(x[k][j]);
+                }
+            }
+
+        }
+        
+        for (int k =0; k < PopulationSize; k++)
+        {
+            for (int p=0;p< PopulationDimention; p++)
+            {
+                std::uniform_real_distribution<> l(0,1);
+                if (l(gen) < MutationProbability)
+                {
+                    float value2;
+                    std::uniform_real_distribution<> m_temp(Minimum_Range[0], Maximum_Range[0]);
+                    value2 = m_temp(gen);
+                    z[k][p] = value2; 
+                }
+            }
+        }
+        x.clear();
+        for (int i=0; i< PopulationSize; i++)
+        {
+            x.push_back(z[i]);
+        }
+
+        Cost=Cost_Function(x, PopulationSize, PopulationDimention);
+        Data_Sort(x, Cost, PopulationSize);
+
+        for (int l =0; l < NumberOfElites; l++)
+        {
+            x[PopulationSize-l+1] = EliteSolutions[l];
+            Cost[PopulationSize-l+1] = EliteCosts[l];
+        }
+
+        Data_Sort(x, Cost, PopulationSize);
+        Minimum_Range.push_back(Cost[0]);
+        std::cout << "Generation " << crnt_generation << " min cost= "<< Cost[0];
+    }
 }
 
 std::vector<float> Cost_Function(std::vector<std::vector<float>> initial_m, int Population_Size, int Population_Dimention){
