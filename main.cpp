5cb835017909d031e4b731e461b28f446bda5cb7
Arash Roshanineshat <a.roshanineshat@vikes.csuohio.edu>
2018-03-01 02:53:35 -0500

    sorting finished

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <cmath>
 
 std::vector<float> Cost_Function(std::vector<std::vector<float>>, int, int);
-void Data_Sort(std::vector<std::vector<float>>*, std::vector<float>*);
+void Data_Sort(std::vector<std::vector<float>>&, std::vector<float>&, int);
 
 int main (int argc, char* argv[])
 {
@@ -40,7 +40,7 @@ int main (int argc, char* argv[])
     }
 
     std::vector<float> Cost=Cost_Function(x, PopulationSize, PopulationDimention);
-    
+    Data_Sort(x, Cost, PopulationSize);
 
 }
 
@@ -59,4 +59,29 @@ std::vector<float> Cost_Function(std::vector<std::vector<float>> initial_m, int 
         Cost_temp.push_back(Cost_eachpop);
     }
     return Cost_temp;
+}
+
+bool comparator ( const std::pair<float,int>& l, const std::pair<float,int>& r)
+       { return l.first < r.first; }
+
+void Data_Sort(std::vector<std::vector<float>> &matrix, std::vector<float> (&Cost), int Population_Size)
+{
+    std::vector<std::pair<float,int>> New_vector;
+    std::vector<std::vector<float>> New_matrix;
+    for (int i = 0; i < Population_Size; i++)
+    {
+        New_vector.push_back(std::make_pair(Cost[i], i));
+        New_matrix.push_back(matrix[i]);
+    }
+    std::sort(New_vector.begin(), New_vector.end(), comparator);
+    
+    matrix.clear();
+    Cost.clear();
+    for (int i=0; i< Population_Size;i++)
+    {
+        matrix.push_back(New_matrix[New_vector[i].second]);
+        Cost.push_back(New_vector[i].first);
+    }
+    
+
 }
