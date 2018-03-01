84fb6137af3280087142ca69f6bf54908a8734c3
Arash Roshanineshat <a.roshanineshat@vikes.csuohio.edu>
2018-03-01 01:48:07 -0500

    To add sorting

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include <algorithm>
 #include <iostream>
 #include <random>
+#include <cmath>
+
+std::vector<float> Cost_Function(std::vector<std::vector<float>>, int, int);
+void Data_Sort(std::vector<std::vector<float>>*, std::vector<float>*);
 
 int main (int argc, char* argv[])
 {
 
     int GenerationLimit=40;
     int PopulationSize=20;
-    int PopulationDimention=10;
+    int PopulationDimention=50;
     float MutationProbability=0.04;
     int NumberOfElites=2;
 
@@ -19,7 +23,6 @@ int main (int argc, char* argv[])
 
     std::random_device rd;
     std::mt19937 gen(rd());
-//std::uniform_int_distribution<> dis(1,6);
 
     std::vector<std::vector<float>> x;
 
@@ -28,9 +31,32 @@ int main (int argc, char* argv[])
         std::vector<float> temp_row;
         for (int j=0; j< PopulationDimention; j++)
         {
-            float value;
-
+           float value;
+           std::uniform_real_distribution<> disp(Minimum_Range[0], Maximum_Range[0]);
+           value = disp(gen);
+           temp_row.push_back(value);
         }
+        x.push_back(temp_row);
     }
+
+    std::vector<float> Cost=Cost_Function(x, PopulationSize, PopulationDimention);
     
+
+}
+
+std::vector<float> Cost_Function(std::vector<std::vector<float>> initial_m, int Population_Size, int Population_Dimention){
+
+    std::vector<float> Cost_temp;
+
+    for (int current_pop=0; current_pop<Population_Size; current_pop++){
+        float Cost_eachpop=0;
+        for (int current_dimention=0; current_dimention<Population_Dimention-1; current_dimention++)
+        {
+            float temp1 = initial_m[current_pop][current_dimention];
+            float temp2 = initial_m[current_pop][current_dimention+1];
+            Cost_eachpop=Cost_eachpop + 100*pow(temp2-pow(temp1,2),2) + pow(temp1-1,2);
+        }
+        Cost_temp.push_back(Cost_eachpop);
+    }
+    return Cost_temp;
 }
